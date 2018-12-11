593c10d0a40935bee9b909b8e237200db2581317
wpalafox <wpalafox@gmail.com>
2018-12-10 18:26:44 -0600

    rendering repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -32,8 +32,36 @@ function SelectLanguage (props){
 }
 
 
+function RepoGrid (props){
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function (repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
 
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
 
+}
 
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
@@ -102,8 +130,12 @@ class Popular extends React.Component {
           <SelectLanguage 
             selectedLanguage={this.state.selectedLanguage}
             onSelect={this.updateLanguage} /> 
+            {!this.state.repos
+              ? <p>LOADING</p>
+              :<RepoGrid repos={this.state.repos} />}
 
-           
+            }
+           />
         
          </div>
        

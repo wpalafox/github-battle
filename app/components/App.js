3515243fa4082910de6693aef1523b72baa60ae8
wpalafox <wpalafox@gmail.com>
2018-12-11 05:24:37 -0600

    added router and nav bar

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,22 @@
 var React = require('react')
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom')
+var Router = ReactRouter.BrowserRouter; 
+var Route = ReactRouter.Route; 
+var Nav = require('./Nav')
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-          <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+        <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+        
+      </Router> 	
+    
     )
   }
 }
